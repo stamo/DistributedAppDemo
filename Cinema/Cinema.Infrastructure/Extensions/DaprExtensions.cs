ca7d7522ed78e21b7a9fcab6485e2eaff405cf4f
Стамо Петков <sgpetkov@is-bg.net>
2025-04-07 16:34:24 +0300

    Extensions added

---
+++ Cinema/Cinema.Infrastructure/Extensions/DaprExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Dapr.Client;
+using Dapr.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class DaprExtensions
+    {
+        public static IHostApplicationBuilder ConfigureDapr(
+            this IHostApplicationBuilder builder,
+            string? configComponentName = null,
+            IReadOnlyList<string>? configKeys = null,
+            string? secretComponentName = null)
+        {
+            DaprClient client = new DaprClientBuilder().Build();
+            builder.Services.TryAddSingleton(client);
+
+            if (string.IsNullOrEmpty(configComponentName) == false
+                && configKeys != null && configKeys.Count() > 0)
+            {
+                builder.Configuration.AddDaprConfigurationStore(
+                    configComponentName, 
+                    configKeys, 
+                    client, 
+                    TimeSpan.FromSeconds(20));
+            }
+
+            if (string.IsNullOrEmpty(secretComponentName) == false)
+            {
+                builder.Configuration.AddDaprSecretStore(
+                    secretComponentName, 
+                    client, 
+                    TimeSpan.FromSeconds(10));
+            }
+
+            return builder;
+        }
+    }
+}
---
+++ Cinema/Cinema.Infrastructure/Extensions/TelemetryExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class TelemetryExtensions
+    {
+        public static IHostApplicationBuilder ConfigureOpenTelemetry(this IHostApplicationBuilder builder)
+        {
+            builder.Logging.AddOpenTelemetry(logging =>
+            {
+                logging.IncludeFormattedMessage = true;
+                logging.IncludeScopes = true;
+            });
+
+            builder.Services.AddOpenTelemetry()
+                .WithMetrics(metrics =>
+                {
+                    metrics.AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation();
+                })
+                .WithTracing(tracing =>
+                {
+                    tracing.AddZipkinExporter()
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation();
+                });
+
+
+            return builder;
+        }
+    }
+}
