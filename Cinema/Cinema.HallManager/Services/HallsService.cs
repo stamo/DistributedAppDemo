788b77a267a67d8fbceb5c3b79c481cd168e3fee
Стамо Петков <sgpetkov@is-bg.net>
2025-04-07 15:03:46 +0300

    HallsService configured

---
+++ Cinema/Cinema.HallManager/Services/HallsService.cs
@@ -0,0 +1,14 @@
+﻿using Dapr.AppCallback.Autogen.Grpc.v1;
+
+namespace Cinema.HallManager.Services
+{
+    public class HallsService : AppCallback.AppCallbackBase
+    {
+        private readonly ILogger logger;
+
+        public HallsService(ILogger<HallsService> _logger)
+        {
+            logger = _logger;
+        }
+    }
+}
